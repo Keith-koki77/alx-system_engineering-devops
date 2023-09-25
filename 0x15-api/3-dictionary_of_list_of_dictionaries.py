eef287a6c214baf2efc7529f4f1425a1c4e1542f
Keith koki <kokikeith@gmail.com>
2023-09-25 15:15:00 -0700

    file fixed

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -5,63 +5,66 @@ Python script to export data in the JSON format
 
 import json
 import requests
+from sys import argv
+
+
+def fetch_employee_data(employee_id):
+    base_url = 'https://jsonplaceholder.typicode.com/'
+    user_url = f'{base_url}users/{employee_id}'
+    todos_url = f'{base_url}users/{employee_id}/todos'
+
+    response_user = requests.get(user_url)
+    response_todos = requests.get(todos_url)
+
+    if response_user.status_code != 200:
+        print(f"Failed to fetch user data for ID {employee_id}")
+        return None, None
+
+    user_data = response_user.json()
+    todos_data = response_todos.json()
+
+    return user_data, todos_data
+
+
+def generate_employee_json(employee_id, user_data, todos_data):
+    if not user_data or not todos_data:
+        return None
+
+    username = user_data.get('username', 'user name not found')
+
+    records = {
+        employee_id: [
+            {
+                "username": username,
+                "task": todo.get('title'),
+                "completed": todo.get('completed')
+            }
+            for todo in todos_data
+        ]
+    }
+
+    return records
+
+
+def save_to_json_file(data, filename):
+    if not data:
+        return
+
+    with open(filename, 'w') as jsonfile:
+        json.dump(data, jsonfile, indent=4)
 
 
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    users = requests.get(url + "users").json()
-    todos = requests.get(url + "todos").json()
+    if len(argv) != 1:
+        print("Usage: python script.py")
+    else:
+        all_records = {}
+        for employee_id in range(1, 11):  # Assuming employee IDs from 1 to 10
+            user_data, todos_data = fetch_employee_data(employee_id)
+            employee_json_data = generate_employee_json(
+                employee_id, user_data, todos_data)
+            if employee_json_data:
+                all_records.update(employee_json_data)
 
-    """
-    Create a dictionary to store task data for all employees
-    """
-    all_employee_tasks = {}
-
-    for user in users:
-        user_id = user.get("id")
-        username = user.get("username")
-
-        """
-        Filter tasks for the current employee
-        """
-        employee_tasks = [todo for todo in todos if
-                          todo.get("userId") == user_id]
-
-        """
-        Create a list to store task data for the current employee
-        """
-        task_data = []
-
-        for task in employee_tasks:
-            task_title = task.get("title")
-            completed = task.get("completed")
-
-            """
-            Create a dictionary for each task
-            """
-            task_info = {
-                "username": username,
-                "task": task_title,
-                "completed": completed
-            }
-
-            """Append the task info to the list"""
-            task_data.append(task_info)
-
-        """
-        Add the task data for the current employee to the dictionary
-        """
-        all_employee_tasks[user_id] = task_data
-
-    """
-    Define the file name as todo_all_employees.json
-    """
-    file_name = "todo_all_employees.json"
-
-    """
-    Write the JSON data to the file
-    """
-    with open(file_name, "w") as json_file:
-        json.dump(all_employee_tasks, json_file, indent=4)
-
-    print("Data exported to {}".format(file_name))
+        if all_records:
+            save_to_json_file(all_records, 'todo_all_employees.json')
