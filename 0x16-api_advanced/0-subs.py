ee6d6b016900774145fe6504f87cad91b3beac3e
Keith koki <kokikeith@gmail.com>
2023-10-10 13:18:17 -0700

    file updated

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+"""
+
+import requests
+import sys
+
+
+def number_of_subscribers(subreddit):
+    """
+    gets the number of subs of subreddit
+    """
+    headers = {'User-agent': 'test'}
+    subs = requests.get('https://www.reddit.com/r/{}/about.json'.format(
+        sys.argv[1]), allow_redirects=False, headers=headers)
+
+    if subs.status_code == 200:
+        return (subs.json()['data']['subscribers'])
+    else:
+        return 0
---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Python Script for Task 1
+"""
+
+import requests
+import sys
+
+def top_ten(subreddit):
+    """
+    Get 10 hottest posts of a subreddit
+    """
+    headers = {'User-agent': 'test23'}
+    url = 'https://www.reddit.com/r/'
+    posts = requests.get(url + '{}/hot.json?limit=10'.format(
+        sys.argv[1]), allow_redirects=False, headers=headers)
+
+    if posts.status_code == 200:
+        for post in posts.json()['data']['children']:
+            print(post['data']['title'])
+
+    else:
+        print(None)
