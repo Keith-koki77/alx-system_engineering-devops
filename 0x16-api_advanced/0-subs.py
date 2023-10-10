75f73b06c3aadfb3c7a1a23b7b7a13a8e64ac78c
Keith koki <kokikeith@gmail.com>
2023-10-10 14:34:29 -0700

    file fixed

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """
+Python Script that shows the number_of_subscribers.
 """
 
 import requests
@@ -10,11 +11,11 @@ def number_of_subscribers(subreddit):
     """
     gets the number of subs of subreddit
     """
-    headers = {'User-agent': 'test'}
-    subs = requests.get('https://www.reddit.com/r/{}/about.json'.format(
-        sys.argv[1]), allow_redirects=False, headers=headers)
 
-    if subs.status_code == 200:
-        return (subs.json()['data']['subscribers'])
-    else:
+    if subreddit is None or type(subreddit) is not str:
         return 0
+    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+                      headers={'User-Agent': '0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
+    subs = r.get("data", {}).get('subscribers', 0)
+    return subs
